9cdff674046c7b5bc4081ba776c4eb5bd5207f5c
Anurag-Bhatt <anuragbhatt865@gmail.com>
2026-01-17 14:43:14 +0530

    Feat: Integrate spdlog and implement Log wrapper

--- Yantra-Core/src/Yantra/Application.cpp
+++ Yantra-Core/src/Yantra/Application.cpp
@@ -1,10 +1,13 @@
 #include "Yantra/Application.h"
-#include <cstdio>
+#include "Yantra/Log.h"
 
 namespace Yantra{
 
     Application::Application()
     {
+        Log::Init();
+        YANTRA_CORE_WARN("Yantra Initialized\n");
+        YANTRA_INFO("Client says Hello\n");
     }
 
     Application::~Application()
---
+++ Yantra-Core/src/Yantra/Log.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <Yantra/Core.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
+#include <memory>
+
+namespace Yantra {
+
+	class YANTRA_API Log {
+
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger(){ return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger(){ return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+
+// Core log macros
+#define YANTRA_CORE_TRACE(...)			::Yantra::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define YANTRA_CORE_INFO(...)			::Yantra::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define YANTRA_CORE_WARN(...)			::Yantra::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define YANTRA_CORE_ERROR(...)			::Yantra::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define YANTRA_CORE_FATAL(...)			::Yantra::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define YANTRA_TRACE(...)				::Yantra::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define YANTRA_INFO(...)				::Yantra::Log::GetClientLogger()->info(__VA_ARGS__)
+#define YANTRA_WARN(...)				::Yantra::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define YANTRA_ERROR(...)				::Yantra::Log::GetClientLogger()->error(__VA_ARGS__)
+#define YANTRA_FATAL(...)				::Yantra::Log::GetClientLogger()->fatal(__VA_ARGS__)
