abca518a197e34b9379e86552d290225a5e49099
Anurag-Bhatt <anuragbhatt865@gmail.com>
2026-01-23 19:10:22 +0530

    Added glm as a submodule

--- Yantra-Core/src/Yantra.h
+++ Yantra-Core/src/Yantra.h
@@ -5,5 +5,9 @@
 #include "Yantra/Layer.h"
 #include "Yantra/Log.h"
 
+#include "Yantra/Input.h"
+
+#include "Yantra/ImGui/ImGuiLayer.h"
+
 // ----- Entry Point --------
 #include "Yantra/EntryPoint.h"
